67c78d36db2693704eee42afc75c9defe6253aa2
Georg Nieuwoudt <georg.nieu@gmail.com>
2022-10-17 21:34:36 +0200

    init shell script formatting

--- docker/ftp/init.sh
+++ docker/ftp/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for user_home in /home/* ; do
+for user_home in /home/*; do
   if [ -d "$user_home" ]; then
-    username=`basename $user_home`
+    username=$(basename $user_home)
     echo "Setup $user_home/share folder for $username"
     mkdir -p $user_home/share
     chown -R $username:users $user_home/share
